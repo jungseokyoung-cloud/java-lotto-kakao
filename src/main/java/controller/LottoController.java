46c8d7d67475e12c1d3789c45f29a82d20ff98df
cheesecrust <jack0810@naver.com>
2026-02-10 15:07:41 +0900

    feat: 수익률 출력 메서드

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -20,6 +20,8 @@ public class LottoController {
         List<Rank> ranks = lottoTickets.match(winningLotto, bonusNumber);
         System.out.println(ranks.size());
         OutputView.printWinning(ranks);
+
+        System.out.printf("총 수익률은 %f입니다.", userMoney.calculateRate(ranks));
     }
 
     private static <T> T retry(Supplier<T> supplier) {
