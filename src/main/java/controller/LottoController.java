0ecbbdb44d443177f6660f80f6ed9ef4e1874d06
rui.bao <thrdud0423@gmail.com>
2026-02-10 11:24:34 +0900

    feat: 당첨 번호 입력 메서드 (String 반환 -> ,로 분리 및 파싱)

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import domains.Lotto;
 import domains.Money;
 import view.InputView;
 
+import java.util.List;
+
 public class LottoController {
     public static void run() {
-        Money userMoney = new Money(InputView.inputMoney());
+        Money userMoney = InputView.inputMoney();
 
+        Lotto winningLotto = InputView.inputWinningNumbers();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,32 @@
 package view;
 
+import domains.Lotto;
+import domains.LottoNumber;
+import domains.Money;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static Integer inputMoney() {
+    public static Money inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(scanner.nextLine());
+        int value = Integer.parseInt(scanner.nextLine());
+        return new Money(value);
+    }
+
+    public static Lotto inputWinningNumbers() {
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+        String input = scanner.nextLine();
+        List<LottoNumber> numbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        return new Lotto(numbers);
     }
 }
