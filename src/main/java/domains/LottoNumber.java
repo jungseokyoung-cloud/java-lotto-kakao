b6f1d83fb1af87c56d1f659f5d7e87d66811eba1
rui.bao <thrdud0423@gmail.com>
2026-02-11 13:58:19 +0900

    refact: LottoNumber를 class에서 record로 변경

--- src/main/java/domains/LottoNumber.java
+++ src/main/java/domains/LottoNumber.java
@@ -1,33 +1,14 @@
 package domains;
 
-import java.util.Objects;
-
-public class LottoNumber implements Comparable<LottoNumber> {
+public record LottoNumber(Integer number) implements Comparable<LottoNumber> {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
-
-    private final Integer number;
 
     public LottoNumber(Integer number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1~45사이여야 합니다.");
         }
         this.number = number;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        LottoNumber that = (LottoNumber) o;
-        return number.equals(that.number);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number);
     }
 
     @Override
